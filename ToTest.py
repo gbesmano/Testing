5d38069533ebe649ed54861603046e7727c52d26
Glen Besmano <57648416+gbesmano@users.noreply.github.com>
2024-04-18 08:48:34 +0800

    Add files via upload

---
+++ ToTest.py
@@ -0,0 +1,19 @@
+fave_fruits  = ["apple", "mango", "banana", "grapes", "pomelo", "dragon-fruit", "orange" ]
+
+print(fave_fruits) # []
+print(fave_fruits[0]) # s
+print(fave_fruits[1]) # s
+print(fave_fruits[-1]) # s
+print(fave_fruits[1:3]) # []
+print(len(fave_fruits))
+print(fave_fruits[2:4]) 
+print(fave_fruits[-3:-2])
+print(fave_fruits[3:])
+print(fave_fruits[-4:])
+print(fave_fruits[:5])
+
+print(fave_fruits[-4])
+
+SECRET_KEY = 'ewmacin13ib5n-ws0&2p2!10_d=j=8c9n_+kfcuqvifqcng(vs'
+Username = 'Trisha'
+Password = 'Gigantone'
