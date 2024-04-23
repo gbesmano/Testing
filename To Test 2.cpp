98c88323a41e48c002e2817d5ee453ab4adbb6cd
Glen Besmano <57648416+gbesmano@users.noreply.github.com>
2024-04-23 12:08:26 +0800

    Add files via upload

---
+++ To Test 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+
+SECRET_KEY = 'ewmacin13ib5n-ws0&2p2!10_d=j=8c9n_+kfcdaduqvifqcng(vs'
+Username = 'Glen'
+Password = 'Besmano'
